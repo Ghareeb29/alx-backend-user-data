5c72d1b1425cb16639558c4fcc1d1075f8fc3a9d
Ghareeb29 <sonic_unleashed@live.com>
2024-09-03 19:25:06 +0300

    Task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,18 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """require_auth"""
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or excluded_paths == []:
+            return True
+        if path[-1] != "/":
+            path += "/"
+        for ex_path in excluded_paths:
+            if ex_path[-1] != "/":
+                ex_path += "/"
+            if path == ex_path:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """authorization_header"""
