9c81aa7b526a52587d3813efd768768e56bda7c3
Ghareeb29 <sonic_unleashed@live.com>
2024-09-06 17:48:54 +0300

    task 3

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -30,3 +30,22 @@ class SessionAuth(Auth):
             session_id = str(uuid.uuid4())
             self.user_id_by_session_id[session_id] = user_id
             return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Returns the user ID associated with the session ID.
+
+        Args:
+            session_id (str): The ID of the session.
+            Defaults to None.
+
+        Returns:
+            str: The ID of the user associated with the session,
+            or None if session_id is None or not a string.
+        """
+        if session_id is None:
+            return None
+        elif type(session_id) is not str:
+            return None
+        else:
+            return self.user_id_by_session_id.get(session_id)
