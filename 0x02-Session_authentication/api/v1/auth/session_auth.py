434a19fa58acc93d08ccbd737f04a5507ee04d90
Ghareeb29 <sonic_unleashed@live.com>
2024-09-06 04:31:31 +0300

    added docstring to method create_session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -11,6 +11,17 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
+        """
+        Creates a new session and returns the session ID.
+
+        Args:
+            user_id (str): The ID of the user
+            to create a session for. Defaults to None.
+
+        Returns:
+            str: The ID of the created session,
+            or None if user_id is None or not a string.
+        """
         if user_id is None:
             return None
         elif type(user_id) is not str:
