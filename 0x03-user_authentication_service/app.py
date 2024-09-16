b2280b3b507a870f59d0ff4cfc63e9fe8d8a4b4b
Ghareeb29 <sonic_unleashed@live.com>
2024-09-16 04:23:54 +0300

    Task 7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,20 +3,41 @@
 Flask App
 """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort, redirect
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/")
 def welcome():
     """
     Defines a route for the root URL ("/") of the Flask application.
-    
+
     Returns a JSON response with a welcome message.
     """
-    
+
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def users():
+    """POST /users
+    JSON body:
+      - email
+      - password
+    Return:
+      - User object JSON represented
+      - 400 if can't create the new User
+    """
+    try:
+        email = request.form["email"]
+        password = request.form["password"]
+        user = AUTH.register_user(email, password)
+        return jsonify(user.to_json())
+    except Exception as e:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
